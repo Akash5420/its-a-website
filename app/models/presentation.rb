e1e5da50a843422fbd911df448dd314ac7ba7430
Akash5420 <72155229+Akash5420@users.noreply.github.com>
2020-09-30 22:24:29 +0530

    Update presentation.rb

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -2,6 +2,8 @@ class Presentation < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :starts_at
   validates_presence_of :speaker_name
+  validates_presence_of :boss_name
+  validates_presence_of :leader_name
   
   has_many :presentation_questions
 end
